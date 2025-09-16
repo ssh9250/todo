28159eb5a0fabb29c5f9ed5bb0898dd3766e045c
ssh9250 <sh9250r@gmail.com>
2025-09-16 22:05:46 +0900

    refactor: Serializer 세분화 및 url 경로 재설정 - CRUD 별 Serializer 작성 - View 에서 Serializer 재할당

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.TodoListCreateView.as_view(), name='todo-list-create'),
     path('<int:pk>/', views.TodoDetailView.as_view(), name = 'todo-detail'),
-    path('<int:pk>//toggle/', views.todo_toggle, name = 'todo-toggle'),
+    path('<int:pk>/toggle/', views.todo_toggle, name = 'todo-toggle'),
     path('stats/', views.todo_stats, name = 'todo-stats'),
 
 ]
