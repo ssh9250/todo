55113658e9fdbe9b6a86a5ca5bedcd9ef6636016
ssh9250 <sh9250r@gmail.com>
2025-10-02 16:54:08 +0900

    feat: Todo API에 페이지네이션 및 통계 기능 추가 - Todo 목록 조회에 페이지네이션 기능 구현 - 숨김 처리된 Todo 포함 조회 및 통계 집계 최적화 - 새로운 API 엔드포인트 추가: 모든 Todo 조회 및 숨김 Todo 조회

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q, Count
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Todo
 from .serializers import (
@@ -13,21 +15,26 @@ from .serializers import (
 
 # mixin 패턴으로 공통 기능 분리
 
+class TodoPagination(PageNumberPagination):
+    """할 일 목록 페이지네이션 설정"""
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 
 class TodoViewSet(viewsets.ModelViewSet):
     """Todo CRUD 및 추가 액션을 제공하는 ViewSet"""
+    pagination_class = TodoPagination
     # 필터링/검색/정렬 기능 추가 예정
 
-    # Query Params for filtering hidden todos
     def get_queryset(self):
-        queryset = Todo.objects.all()
+        """기본적으로 숨김 처리되지 않은 todo만 반환"""
+        include_hidden = self.request.query_params.get('include_hidden', 'false').lower()
+        base_queryset = Todo.objects.all()
         
-        include_hidden = self.request.query_params.get('include_hidden', 'false')
         if include_hidden == 'true':
-            return queryset
-        return queryset.filter(hidden=False)
-
-    queryset = Todo.objects.all()
+            return base_queryset
+        return base_queryset.filter(hidden=False)
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -40,22 +47,71 @@ class TodoViewSet(viewsets.ModelViewSet):
             return TodoUpdateSerializer
         return TodoDetailSerializer
 
-    """쿼리 파라미터"""
     def list(self, request, *args, **kwargs):
+        """할 일 목록 조회 (성능 최적화된 통계 및 페이지네이션 포함)"""
         queryset = self.get_queryset()
-        # 성능 최적화 필요 (aggregate 사용 / paginate_queryset 사용 / get_paginated_response 사용)
+        
+        # 단일 쿼리로 모든 통계 계산 (성능 최적화)
         stats = queryset.aggregate(
             total_count=Count('id'),
             completed_count=Count('id', filter=Q(completed=True)),
+            pending_count=Count('id', filter=Q(completed=False)),
         )
+        
+        # 페이지네이션 적용
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response({
+                'results': serializer.data,
+                'stats': {
+                    'total_count': stats['total_count'],
+                    'completed_count': stats['completed_count'],
+                    'pending_count': stats['pending_count'],
+                }
+            })
+        
+        # 페이지네이션이 비활성화된 경우
         serializer = self.get_serializer(queryset, many=True)
         return Response(
             {
                 'results': serializer.data,
                 'total_count': stats['total_count'],
                 'completed_count': stats['completed_count'],
+                'pending_count': stats['pending_count'],
             }
         )
+    
+    @action(detail=False, methods=['get'], url_path='all')
+    def list_all(self, request):
+        """모든 할 일 조회 (숨김 처리된 것 포함)"""
+        queryset = Todo.objects.all()
+        
+        # 단일 쿼리로 통계 계산 (성능 최적화)
+        stats = queryset.aggregate(
+            total_count=Count('id'),
+            hidden_count=Count('id', filter=Q(hidden=True)),
+            completed_count=Count('id', filter=Q(completed=True)),
+        )
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'results': serializer.data,
+            'total_count': stats['total_count'],
+            'hidden_count': stats['hidden_count'],
+            'completed_count': stats['completed_count'],
+            'hidden_included': True,
+        })
+
+    @action(detail=False, methods=['get'], url_path='hidden')
+    def list_hidden(self, request):
+        """숨김 처리된 할 일만 조회"""
+        queryset = Todo.objects.filter(hidden=True)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'results': serializer.data,
+            'total_count': queryset.count(),
+        })
 
     @action(detail=True, methods=['patch'], url_path='toggle')
     def toggle(self, request, pk=None):
@@ -68,18 +124,26 @@ class TodoViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'], url_path='stats')
     def stats(self, request):
-        """GET /api/todos/stats/ - 통계 정보"""
+        """GET /api/todos/stats/ - 통계 정보 (성능 최적화)"""
         todos = self.get_queryset()
+        
+        # 단일 쿼리로 모든 통계 계산 (성능 최적화)
+        stats = todos.aggregate(
+            total_count=Count('id'),
+            completed_count=Count('id', filter=Q(completed=True)),
+            pending_count=Count('id', filter=Q(completed=False)),
+        )
+        
         return Response({
-            'total_count': todos.count(),
-            'completed_count': todos.filter(completed=True).count(),
-            'pending_count': todos.filter(completed=False).count(),
+            'total_count': stats['total_count'],
+            'completed_count': stats['completed_count'],
+            'pending_count': stats['pending_count'],
         })
 
     @action(detail=True, methods=['patch'], url_path='hide')
     def hide(self, request, pk=None):
-        todo = get_object_or_404(Todo, pk=pk)
-        # todo = self.get_object()
+        """PATCH /api/todos/{id}/hide/ - 숨김 상태 토글"""
+        todo = self.get_object()
         todo.hidden = not todo.hidden
         todo.save()
         serializer = TodoDetailSerializer(todo)
