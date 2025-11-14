60cb588f541bbdf10497aeba393091a1e95ba465
ssh9250 <sh9250r@gmail.com>
2025-11-14 21:28:04 +0900

    test commit

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -9,7 +9,7 @@ router.register('', TodoViewSet, basename='todo')
 urlpatterns = [
     path('', include(router.urls)),
 ]
-# http://localhost:8000/api/todos/ 테스트 해보기
+# http://localhost:8000/api/todos/ 테스트 해보기ㅇㄹㅇㄹㅇㄹ
 
 
 '''
